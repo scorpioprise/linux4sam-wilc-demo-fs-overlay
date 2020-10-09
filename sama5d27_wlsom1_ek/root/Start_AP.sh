e216a6b5ff72fa5478d90b6d4131d31ae97d623f
C21415 <hemanth.tenkod@microchip.com>
2020-10-08 23:41:39 -0700

    debug print changes

--- sama5d27_wlsom1_ek/root/Start_AP.sh
+++ sama5d27_wlsom1_ek/root/Start_AP.sh
@@ -18,27 +18,29 @@ echo " #     # #          ######  ###### #    #  ####    "
 echo "---------------------------------------------------"
 
 if lsmod | grep -q "wilc_sdio" ; then
-	echo "1. WILC-SDIO module is available"
+	echo "1.############## WILC-SDIO module is available ##############"
 else
 	echo "1. Inserting the wilc-sdio module" 
 	modprobe wilc-sdio > wilc-sdio_module.log
-	if grep -q "Driver Initializing success" wilc-sdio_module.log ; then
+	if lsmod | grep -q "wilc_sdio";  then
 		echo "WILC-SDIO module insterted successfully"
 	else
-		echo "WILC-SDIO module insert failed, check wilc-sdio_module.log file"
+		echo "WILC-SDIO module insert failed"
 	fi
 fi
 
-echo "2. Bringing up the wlan0 interface" 
-ifconfig wlan0 up > wlan0_up.log
+
+
+echo "2.############## Bringing up the wlan0 interface ##############" 
+ifconfig wlan0 up
 if ifconfig | grep -q "wlan0" ; then
 	echo "Wireless LAN interface is UP!"
 else
 	echo "Wireless LAN interface has FAILED, check wlan0_up.log file"
 fi
 
-echo "3. Starting the Host AP deamon" 
-hostapd /etc/wilc_hostapd_open.conf -B & > hostapd.log
+echo "3.############## Starting the Host AP deamon ##############" 
+hostapd /etc/wilc_hostapd_open.conf -B &
 if ps | grep -q "hostapd" ; 
 then
 	echo "hostapd process has started successfully"
@@ -60,17 +62,16 @@ subnet 192.168.1.0 netmask 255.255.255.0 {
 EOT
 fi
 
-echo "4. Configuring the AP IP Address to 192.168.1.1"
+echo "4.############## Configuring the AP IP Address to 192.168.1.1 ##############"
 ifconfig wlan0 192.168.1.1 
-echo "5. Configuring the NGINX Webserver"
+echo "5. ############## Configuring the NGINX Webserver ##############"
 killall nginx
 sed -i "s/\broot\b.*/root \/usr\/share\/nginx\/html;/g" /etc/nginx/sites-available/default_server
 nginx &
  
-echo "6. Starting the DHCP server"
-#/etc/init.d/dhcp-server start &
+echo "6.############## Starting the DHCP server ##############"
 dhcpd -cf /etc/dhcp/dhcpd.conf 
-echo "7. Starting the WEB scoket deamon"
+echo "7.############## Starting the WEB scoket deamon ##############"
 cd /root
 ./websocket & 
 echo "Now, The device comes up as an Access Point(AP) and host a webpage to provision"
@@ -78,4 +79,5 @@ echo "WiFi station interface"
 echo "\n"
 echo "Use a Phone/Laptop and connect to the 'wilc1000_SoftAP' WiFi AP"
 echo "Using the web browser open http://192.168.1.1"
-
+echo "---------------------------------------------------"
+echo "---------------------------------------------------"
