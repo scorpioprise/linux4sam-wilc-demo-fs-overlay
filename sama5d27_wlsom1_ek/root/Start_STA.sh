35e92482541181b7e8c2831d8b849129ec948a60
C21415 <hemanth.tenkod@microchip.com>
2020-10-08 23:42:49 -0700

    Added banner and debug messages

--- sama5d27_wlsom1_ek/root/Start_STA.sh
+++ sama5d27_wlsom1_ek/root/Start_STA.sh
@@ -1,3 +1,37 @@
 #!/bin/sh
-modprobe wilc-sdio
+echo "---------------------------------------------------"
+echo "  #####  #######    #            "
+echo " #     #    #      # #           "
+echo "  #          #     #   #         "
+echo "  #####     #    #     #         "
+echo "       #    #    #######         "
+echo " #     #    #    #     #         "
+echo "  #####     #    #     #         "
+echo "---------------------------------------------------"
+echo " ######  ####### #     # ####### "
+echo " #     # #       ##   ## #     # "
+echo " #     # #       # # # # #     # "
+echo " #     # #####   #  #  # #     # "
+echo " #     # #       #     # #     # "
+echo " #     # #       #     # #     # "
+echo " ######  ####### #     # ####### "
+echo "---------------------------------------------------"
+echo "---------------------------------------------------"
+                                 
+
+if lsmod | grep -q "wilc_sdio" ; then
+        echo "1.############## WILC-SDIO module is available ##############"
+else
+        echo "1.############## Inserting the wilc-sdio module ##############" 
+        modprobe wilc-sdio > wilc-sdio_module.log
+        if lsmod | grep -q "wilc_sdio";  then
+                echo "WILC-SDIO module insterted successfully"
+        else
+                echo "WILC-SDIO module insert failed"
+        fi
+fi
+echo "2.############## Connecting to configured AP ##############"
 ifup wlan0
+echo "---------------------------------------------------"
+echo "---------------------------------------------------"
+
