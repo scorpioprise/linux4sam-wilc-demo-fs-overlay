9192311f1f707bf0d1153231c095434ea86dc911
Giulio Marescotti <giulio.marescotti@rgm.it>
2022-01-17 11:39:57 +0100

    fix: changed issue_command.sh location...

--- sama5d27_wlsom1_ek/usr/html/insert.php
+++ sama5d27_wlsom1_ek/usr/html/insert.php
@@ -1,4 +1,4 @@
 <?php
-$output = exec('/usr/bin/python3 /usr/lib/cgi-bin/issue_command.py 9000');
+$output = exec('/usr/bin/python3 /var/www/localhost/html/cgi-bin/issue_command.py 9000');
 echo $output;
 ?>
