b03282d12d28e4ebf3212c7be74b2013a1674d43
Giulio Marescotti <giulio.marescotti@rgm.it>
2022-03-01 13:32:50 +0000

    fix: fixed cgi path

--- sama5d27_wlsom1_ek/usr/html/reload_index.php
+++ sama5d27_wlsom1_ek/usr/html/reload_index.php
@@ -1,7 +1,7 @@
 <?php
 foreach ( $_SERVER as $k=>$v ) putenv("$k=$v");
 header('Content-type: text/plain');
-$output = shell_exec('sudo --preserve-env /var/www/localhost/html/cgi-bin/store_wifi_parameters.sh 2>&1');
+$output = shell_exec('sudo --preserve-env /usr/share/nginx/html/cgi-bin/store_wifi_parameters.sh 2>&1');
 echo $output;
 // header("refresh:10; url=index.html");
 // exit;
