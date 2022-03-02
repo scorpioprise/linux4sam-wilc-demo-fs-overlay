4e4fa2bfc853366dc86d0ba8731124391c1f64cb
seveso <seveso@hotmail.it>
2022-03-02 17:11:13 +0100

    credenziali db

--- sama5d27_wlsom1_ek/usr/html/inc/config.php
+++ sama5d27_wlsom1_ek/usr/html/inc/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'wallbox');
-define('DB_PASSWORD', 'xm6EnxmP');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'wallbox');
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
