b8231bc10f16fcaeb2d792f436654b82c8e03b63
Giulio Marescotti <giulio.marescotti@rgm.it>
2022-03-02 15:41:30 +0000

    refact: new db schemes with populate db script

--- sama5d27_wlsom1_ek/usr/html/populate_database.sql
+++ sama5d27_wlsom1_ek/usr/html/populate_database.sql
@@ -1,10 +1,59 @@
-CREATE TABLE IF NOT EXISTS cards (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, card_no TEXT(50), name TEXT, tempo DATETIME, status TEXT(50) DEFAULT 'checked', UNIQUE KEY (card_no (50)));
+DROP DATABASE IF EXISTS wallbox;
+CREATE DATABASE wallbox;
+use wallbox;
 
-CREATE TABLE IF NOT EXISTS users (id INT(20) NOT NULL, username VARCHAR(50) PRIMARY KEY, password TEXT, tempo DATETIME, auth INT, UNIQUE KEY (username (50)));
+# #################### TABLE CARDS ##########################
+CREATE TABLE IF NOT EXISTS `cards` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `card_no` varchar(50) NOT NULL,
+  `name` text DEFAULT NULL,
+  `tempo` datetime DEFAULT current_timestamp(),
+  `status` tinytext DEFAULT 'checked',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `card_no` (`card_no`)
+);
 
-CREATE TABLE IF NOT EXISTS transactions (user_id VARCHAR(20), wallbox_status VARCHAR(20), start_time DATETIME, end_time DATETIME, duration VARCHAR(20), delivered_kwh VARCHAR(20), error VARCHAR(20), FOREIGN KEY(user_id) REFERENCES users(username));
 
-CREATE TABLE IF NOT EXISTS configuration (id INT NOT NULL PRIMARY KEY, name VARCHAR(50) NOT NULL, tipo VARCHAR(50), value VARCHAR(50), unit VARCHAR(50), owner VARCHAR(50), UNIQUE (id), UNIQUE (name) );
+# ###################  TABLE USERS ###########################
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `tempo` datetime DEFAULT current_timestamp(),
+  `auth` int(11) DEFAULT NULL,
+  PRIMARY KEY (`username`),
+  UNIQUE KEY `id` (`id`,`username`)
+);
+INSERT INTO `users`(`username`, `password`, `auth`) VALUES ('admin', '$2y$10$dW0ohdRh9ZnAp3Cg8XnofuCB9dFet57lpZWACNG4MpHt5l/i2dDKy', '0'); INSERT INTO `users`(`username`, `password`, `auth`) VALUES ('installer', '$2y$10$qjvVWD0gNabdrGzn5Y//6eHNVZ7yw.0L4EKOs4Tj5tLOnPcLPR4Gi', '1'); INSERT INTO `users`(`username`, `password`, `auth`) VALUES ('user', '$2y$10$TN/KCwotAhKsVJ8vSIuxR.vixTMtC1ME0.UlZs/ZasbCXNeLjPbZm', '2');
+
+# ################### TABLE TRANSACTION ######################
+CREATE TABLE IF NOT EXISTS `transactions` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `tempo` datetime DEFAULT current_timestamp(),
+  `card_no` varchar(20) DEFAULT NULL,
+  `wallbox_status` varchar(20) DEFAULT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `duration` varchar(20) DEFAULT NULL,
+  `delivered_kwh` varchar(20) DEFAULT NULL,
+  `error` varchar(20) DEFAULT NULL,
+  UNIQUE KEY `id` (`id`),
+  KEY `card_no` (`card_no`),
+  CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`card_no`) REFERENCES `cards` (`card_no`) );
+
+##################### TABLE CONFIGURATION ####################
+CREATE TABLE IF NOT EXISTS `configuration` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `tipo` varchar(50) DEFAULT NULL,
+  `value` varchar(50) DEFAULT NULL,
+  `unit` varchar(50) DEFAULT NULL,
+  `owner` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `name` (`name`)
+);
+
 
 INSERT IGNORE INTO configuration VALUES(0,'minimum_current_available_vehicle','float','1.0','Ampere','dsp');
 INSERT IGNORE INTO configuration VALUES(1,'minimum_available_power','float','300.0','Watt','dsp');
@@ -42,4 +91,10 @@ INSERT IGNORE INTO configuration VALUES(32,'wallbox_modbus_address','int','','',
 INSERT IGNORE INTO configuration VALUES(33,'wallbox_ocpp_server_ip_address','uint32_t','','IP','microchip');
 INSERT IGNORE INTO configuration VALUES(34,'wallbox_ocpp_ip_server_port','int','','IP','microchip');
 INSERT IGNORE INTO configuration VALUES(35,'wallbox_ocpp_url_server','string','','url','microchip');
+
+describe users;
+describe configuration;
+describe cards;
+describe transactions;
+
 
