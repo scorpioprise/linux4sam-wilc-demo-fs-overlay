83c0f93f81f6f880f09c8881022a074be3ef0133
seveso <seveso@hotmail.it>
2022-03-04 18:22:55 +0100

    aggiunti porta e socket

--- sama5d27_wlsom1_ek/usr/html/inc/config.php
+++ sama5d27_wlsom1_ek/usr/html/inc/config.php
@@ -3,7 +3,9 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'wallbox');
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+define('DB_PORT', '3306');
+define('DB_SOCKET', '/var/lib/mysql/mysql.sock');
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT, DB_SOCKET);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
