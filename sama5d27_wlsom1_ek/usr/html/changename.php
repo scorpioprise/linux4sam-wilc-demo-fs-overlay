89128392d334912b4eb10aa3bf884520338723ba
seveso <seveso@hotmail.it>
2022-06-13 15:33:13 +0200

    change cardholder name script added

---
+++ sama5d27_wlsom1_ek/usr/html/changename.php
@@ -0,0 +1,4 @@
+<?php
+$var_id = $_REQUEST['oldname']." --- ".$_REQUEST['newname'];
+echo $var_id;
+?>
--- sama5d27_wlsom1_ek/usr/html/index_provisioning.php
+++ sama5d27_wlsom1_ek/usr/html/index_provisioning.php
@@ -118,7 +118,7 @@
               CONTINUE WITHOUT NETWORK
             </button>
             <div class="modal fade" id="withoutNetwork" tabindex="-1" aria-labelledby="withoutNetworkLabel" aria-hidden="true">
-              <div class="modal-dialog">
+              <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                   <div class="modal-header">
                     <h5 class="modal-title" id="withoutNetworkLabel">ARE YOU SURE?</h5>
