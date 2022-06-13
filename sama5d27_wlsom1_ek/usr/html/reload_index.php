fe44981f08e38232b22d09914fe62982e87035d1
Giulio Marescotti <giulio.marescotti@rgm.it>
2022-06-13 12:12:09 +0000

    refact: changed interface to python via issue_command

--- sama5d27_wlsom1_ek/usr/html/reload_index.php
+++ sama5d27_wlsom1_ek/usr/html/reload_index.php
@@ -1,8 +1,6 @@
 <?php
-foreach ( $_SERVER as $k=>$v ) putenv("$k=$v");
-// header('Content-type: text/plain');
-$output = exec('issue_command 9008 ssid=$SSID passphrase=$PASS dhcp=$DHCP ipaddress=$IPADDRESS netmask=$NETMASK gateway=$GATEWAY dns=$DNS');
-echo $output;
-// header("refresh:10; url=index.html");
-// exit;
+$args='';
+foreach ( $_REQUEST as $k=>$v ) $args=$args." $k='$v'";
+$output = exec("issue_command 9008 $args");
+echo "output is ".$output;
 ?>
