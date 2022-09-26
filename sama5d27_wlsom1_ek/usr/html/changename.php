208bfa97f6f1e7dc23afcdfd5af9b97e25c9ff91
Giulio Marescotti <giulio.marescotti@rgm.it>
2022-09-26 12:15:10 +0000

    fix: changename properly call issue_command

--- sama5d27_wlsom1_ek/usr/html/changename.php
+++ sama5d27_wlsom1_ek/usr/html/changename.php
@@ -1,4 +1,6 @@
 <?php
-$var_id = $_REQUEST['oldname']." --- ".$_REQUEST['newname'];
-echo $var_id;
+// $var_id = $_REQUEST['oldname']." --- ".$_REQUEST['newname'];
+// echo $var_id;
+$output = exec('issue_command 3015 '.$_REQUEST['cardnumber']." ".$_REQUEST['newname']);
+echo $output;
 ?>
