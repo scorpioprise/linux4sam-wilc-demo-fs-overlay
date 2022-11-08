9e7d2ced8eb86743670de0079d93b7e48570ab78
seveso <seveso@hotmail.it>
2022-11-08 09:07:49 +0100

    fix variable numerocarta in cards

--- sama5d27_wlsom1_ek/usr/html/cards.php
+++ sama5d27_wlsom1_ek/usr/html/cards.php
@@ -71,7 +71,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         mysqli_stmt_close($stmt);
     }
     if (isset($_POST['responseName'])) {
-        $response = exec('issue_command 3015 ' . $nuovonome . " " . $nuovonome);
+        $response = exec('issue_command 3015 ' . $numerocarta . " " . $nuovonome);
         if ($response == 'RESPONSE_MESSAGE_FAILED') {
             $response_toast = '<div class="toast align-items-center fade show bg-danger fw-bold" role="alert" aria-live="assertive" aria-atomic="true"><div class="d-flex"><div class="toast-body">
             ERRORE - COMANDO NON ESEGUITO</div><button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button></div></div>';
